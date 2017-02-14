ea90e7c3ee3afdd52b4c8e6410190fc689441127
Bayan Berri <bberri@stuy.edu>
2017-02-14 11:12:36 -0500

    comments

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -2,14 +2,16 @@
   class MergeSortTester
 
   ALGORITHM:
-  <INSERT YOUR DISTILLATION OF ALGO HERE>
+  1. if the array length is less than or equal to 1, return the array. (base case for recursion; an array of length one is already sorted). 
+  2. If the length is greater than one, split the array into two, then sort each half(using the same method, so this method is recursive).
+  3. MergeSort.merge your two halves together, which will take your two halves, which are both sorted, and combine them to make a sorted whole. 
 
   BIG-OH CLASSIFICATION OF ALGORITHM:
-  <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
+               O(nlogn)
 
-  Mean execution times for dataset of size n:
+  Mean execution times for dataset of size n: 
   Batch size: <# of times each dataset size was run>
-  n=1       time: 
+  n=1       time: 1
   n=10      time: 
   n=100     time: 
   ...
